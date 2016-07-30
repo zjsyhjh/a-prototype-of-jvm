aabae9fb7ef7887b46bd6a9225bb473efca2b20d
Jiahuan <zjsyhjh@gmail.com>
2016-07-30 17:01:35 +0800

    implement the module of 'cmd parse'

---
+++ src/cmd/cmd.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+    "fmt"
+    "os"
+    "flag"
+)
+
+type Cmd struct {
+   helpFlag bool
+   versionFlag bool
+   classPath string
+   className string
+   classArgs []string
+}
+
+/*
+ * 一般java命令行格式：java [-options] class [args...]
+ *
+ */
+
+func parseCmd() *Cmd {
+    cmd := &Cmd{}
+
+    flag.Usage := printUsage
+    flag.BoolVar(&cmd.helpFlag, "help", "false", "this is help information")
+    flag.BoolVar(&cmd.helpFlag"?", "false", "this is help information")
+    flag.BoolVar(&cmd.versionFlag, "version", "false", "this is version information")
+    flag.String(&cmd.classPath, "classpath", "", "this is class information")
+    flag.String(&cmd.classPath, "cp", "", "this is classpath information")
+    /*
+     * 必须放在标志性参数后面，否则会出现解析错误
+     * 标志性参数如 -cp="xxx", 非标志性参数如 arg1, arg2
+     */
+    args := flag.Parse()
+    
+    if len(args) > 0 {
+        cmd.className := args[0]
+        cmd.classArgs := args[1:]
+    }
+
+    return cmd
+}
+
+func printUsage() {
+    fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0]);
+}
+
+
+
+
+
+
