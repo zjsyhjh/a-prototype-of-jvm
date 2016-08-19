c5b041b51b157a29a7bb0aa10cb944df56e06f4f
Jiahuan <zjsyhjh@gmail.com>
2016-08-19 10:34:29 +0800

    add Test for interpreter

---
+++ src/example/InterpreterTest1.java
@@ -0,0 +1,12 @@
+public class InterpreterTest1 {
+    
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("sum = " + sum);
+    }
+}
