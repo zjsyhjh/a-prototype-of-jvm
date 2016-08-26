e73233be41ce5e7d2963799aa18d766fcf66ec5f
Jiahuan <zjsyhjh@gmail.com>
2016-08-26 17:06:53 +0800

    add invokevirtual instruction

---
+++ src/jvm/instructions/references/invokevirtual.go
@@ -0,0 +1,76 @@
+package references
+
+import (
+	"fmt"
+	"jvm/instructions/base"
+	"jvm/rtda"
+	"jvm/rtda/heap"
+)
+
+/*
+ * 调用所有虚方法
+ * 在运行时确定要调用的方法，它的选择是基于对象类型的
+ */
+type INVOKE_VIRTUAL struct {
+	base.Index16Instruction
+}
+
+func (self *INVOKE_VIRTUAL) Execute(frame *rtda.Frame) {
+	currentClass := frame.Method().Class()
+	cp := currentClass.ConstantPool()
+	methodRef := cp.GetConstant(self.Index).(*heap.MethodRef)
+	resolvedMethod := methodRef.ResolvedMethod()
+
+	if resolvedMethod.IsStatic() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+
+	ref := frame.OperandStack().GetRefFromTop(resolvedMethod.ArgSlotCount() - 1)
+
+	if ref == nil {
+		if methodRef.Name() == "println" {
+			_println(frame.OperandStack(), methodRef.Descriptor())
+			return
+		}
+		panic("java.lang.NullPointerException")
+	}
+
+	if resolvedMethod.IsProtected() &&
+		resolvedMethod.Class().IsSuperClassOf(currentClass) &&
+		resolvedMethod.Class().GetPackageName() != currentClass.GetPackageName() &&
+		ref.Class() != currentClass &&
+		!ref.Class().IsSubClassOf(currentClass) {
+		panic("java.lang.IllegalAccessError")
+	}
+
+	invokedMethod := heap.LookUpMethodInClass(ref.Class(), methodRef.Name(), methodRef.Descriptor())
+	if invokedMethod == nil || invokedMethod.IsAbstract() {
+		panic("java.lang.AbstractMethodError")
+	}
+
+	base.InvokeMethod(frame, invokedMethod)
+}
+
+func _println(stack *rtda.OperandStack, descriptor string) {
+	switch descriptor {
+	case "(Z)V":
+		fmt.Printf("%v\n", stack.PopInt() != 0)
+	case "(C)V":
+		fmt.Printf("%c\n", stack.PopInt())
+	case "(B)V":
+		fmt.Printf("%v\n", stack.PopInt())
+	case "(S)V":
+		fmt.Printf("%v\n", stack.PopInt())
+	case "(I)V":
+		fmt.Printf("%v\n", stack.PopInt())
+	case "(J)V":
+		fmt.Printf("%v\n", stack.PopLong())
+	case "(F)V":
+		fmt.Printf("%v\n", stack.PopFloat())
+	case "(D)V":
+		fmt.Printf("%v\n", stack.PopDouble())
+	default:
+		panic("println : " + descriptor)
+	}
+	stack.PopRef()
+}
