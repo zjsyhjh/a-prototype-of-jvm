48c60726d01a1bb2fe2c70c349fff62ab1e95d56
Jiahuan <zjsyhjh@gmail.com>
2016-08-30 13:06:34 +0800

    update interpreter add logInstruction and logFrames function

--- src/jvm/main/interpreter.go
+++ src/jvm/main/interpreter.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	"fmt"
-	"jvm/classfile"
 	"jvm/instructions"
 	"jvm/instructions/base"
 	"jvm/rtda"
+	"jvm/rtda/heap"
 )
 
-func interpret(methodInfo *classfile.MemberInfo) {
-	codeAttr := methodInfo.CodeAttribute()
-	maxLocals := codeAttr.MaxLocals()
-	maxStack := codeAttr.MaxStack()
-	byteCode := codeAttr.Code()
-
+func interpret(method *heap.Method, logInst bool) {
 	thread := rtda.NewThread()
-	frame := thread.NewFrame(maxLocals, maxStack)
+	frame := thread.NewFrame(method)
 	thread.PushFrame(frame)
 
-	defer catchErr(frame)
-	loop(thread, byteCode)
+	defer catchErr(thread)
+	loop(thread, logInst)
 }
 
-func catchErr(frame *rtda.Frame) {
+func catchErr(thread *rtda.Thread) {
 	if r := recover(); r != nil {
-		fmt.Printf("LocalVars : %v\n", frame.LocalVars())
-		fmt.Printf("OperandStack : %v\n", frame.OperandStack())
+		/*
+			fmt.Printf("LocalVars : %v\n", frame.LocalVars())
+			fmt.Printf("OperandStack : %v\n", frame.OperandStack())
+			panic(r)
+		*/
+		logFrames(thread)
 		panic(r)
 	}
 }
@@ -33,26 +32,49 @@ func catchErr(frame *rtda.Frame) {
 /*
  * loop函数的循环执行: 计算PC， 解码指令， 执行指令
  */
-func loop(thread *rtda.Thread, byteCode []byte) {
-	frame := thread.PopFrame()
+func loop(thread *rtda.Thread, logInst bool) {
 	reader := &base.BytecodeReader{}
 
 	for {
+		frame := thread.CurrentFrame()
 		pc := frame.NextPC()
 		thread.SetPC(pc)
 		/*
 		 * decode
 		 */
-		reader.Reset(byteCode, pc)
+		reader.Reset(frame.Method().Code(), pc)
 		opCode := reader.ReadUint8()
 		inst := instructions.NewInstruction(opCode)
 		inst.FetchOperands(reader)
 		frame.SetNextPC(reader.PC())
 
+		if logInst {
+			logInstruction(frame, inst)
+		}
 		/*
 		 * execute
 		 */
-		fmt.Printf("pc : %2d, inst : %T %v\n", pc, inst, inst)
 		inst.Execute(frame)
+
+		if thread.IsStackEmpty() {
+			break
+		}
+	}
+}
+
+func logInstruction(frame *rtda.Frame, inst base.Instruction) {
+	method := frame.Method()
+	className := method.Class().Name()
+	methodName := method.Name()
+	pc := frame.Thread().PC()
+	fmt.Printf("%v.%v() #%2d %T %v\n", className, methodName, pc, inst, inst)
+}
+
+func logFrames(thread *rtda.Thread) {
+	for !thread.IsStackEmpty() {
+		frame := thread.PopFrame()
+		method := frame.Method()
+		className := method.Class().Name()
+		fmt.Printf(">> pc:%4d %v.%v%v \n", frame.NextPC(), className, method.Name(), method.Descriptor())
 	}
 }
