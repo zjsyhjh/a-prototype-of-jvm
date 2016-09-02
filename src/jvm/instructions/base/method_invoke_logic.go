77c6782be1da3ffb206a5e2577eba6abdfbfa6e2
Jiahuan <zjsyhjh@gmail.com>
2016-09-02 08:57:28 +0800

    add method invoke logic

---
+++ src/jvm/instructions/base/method_invoke_logic.go
@@ -0,0 +1,32 @@
+package base
+
+import (
+	"fmt"
+	"jvm/rtda"
+	"jvm/rtda/heap"
+)
+
+/*
+ * 方法调用, 在定位到需要调用的方法之后，Java虚拟机要给这个方法创建一个新的栈帧并把它推入虚拟机栈顶
+ */
+func InvokeMethod(invokeFrame *rtda.Frame, method *heap.Method) {
+	thread := invokeFrame.Thread()
+	newFrame := thread.NewFrame(method)
+	thread.PushFrame(newFrame)
+
+	argsSlotCount := int(method.ArgSlotCount())
+	if argsSlotCount > 0 {
+		for i := argsSlotCount - 1; i >= 0; i-- {
+			slot := invokeFrame.OperandStack().PopSlot()
+			newFrame.LocalVars().SetSlot(uint(i), slot)
+		}
+	}
+
+	if method.IsNative() {
+		if method.Name() == "registerNatives" {
+			thread.PopFrame()
+		} else {
+			panic(fmt.Sprintf("native method: %v.%v%v\n", method.Class().Name(), method.Name(), method.Descriptor()))
+		}
+	}
+}
