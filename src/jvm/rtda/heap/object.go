074861e4281e25c5c4b249a0cbc2e75dd84bcb3f
Jiahuan <zjsyhjh@gmail.com>
2016-09-03 10:49:57 +0800

    add SetRefVar method

--- src/jvm/rtda/heap/object.go
+++ src/jvm/rtda/heap/object.go
@@ -23,3 +23,9 @@ func (self *Object) Fields() Slots {
 func (self *Object) IsInstanceOf(class *Class) bool {
 	return class.isAssignableFrom(self.class)
 }
+
+func (self *Object) SetRefVar(name, descriptor string, ref *Object) {
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	slots.SetRef(field.slotID, ref)
+}
