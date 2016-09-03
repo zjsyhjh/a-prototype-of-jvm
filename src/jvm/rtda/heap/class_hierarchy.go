9a37164f1ed0ab003151a7cef9e49411caa0d98a
Jiahuan <zjsyhjh@gmail.com>
2016-09-03 10:50:54 +0800

    update isAssignableFrom() method

--- src/jvm/rtda/heap/class_hierarchy.go
+++ src/jvm/rtda/heap/class_hierarchy.go
@@ -13,10 +13,39 @@ func (self *Class) isAssignableFrom(other *Class) bool {
 	if self == other {
 		return true
 	}
-	if !self.IsInterface() {
-		return other.IsSubClassOf(self)
+
+	if !other.IsArray() {
+		if !other.IsInterface() {
+			// other is Class
+			if !self.IsInterface() {
+				//self is Class
+				return other.IsSubClassOf(self)
+			}
+			//self is interface
+			return other.IsImplements(self)
+		} else {
+			//other is interface
+			// self is not interface
+			if !self.IsInterface() {
+				return self.isJlObject()
+			}
+			//self is interface
+			return self.isSuperInterfaceOf(other)
+		}
+
+	} else {
+		//other is array
+		if !self.IsArray() {
+			if !self.IsInterface() {
+				return self.isJlObject()
+			}
+			return self.isJlCloneable() || self.isJioSerializable()
+		} else {
+			c1 := other.ComponentClass()
+			c2 := self.ComponentClass()
+			return c1 == c2 || c2.isAssignableFrom(c1)
+		}
 	}
-	return other.IsImplements(self)
 }
 
 func (self *Class) IsImplements(other *Class) bool {
@@ -41,4 +70,8 @@ func (self *Class) isSubInterfaceOf(other *Class) bool {
 
 func (self *Class) IsSuperClassOf(other *Class) bool {
 	return other.IsSubClassOf(self)
+}
+
+func (self *Class) isSuperInterfaceOf(other *Class) bool {
+	return other.isSubInterfaceOf(self)
 }
