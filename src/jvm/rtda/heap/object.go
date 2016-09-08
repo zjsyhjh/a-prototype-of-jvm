47e630e79cce62c67ca17f6075a9e95884896bc0
Jiahuan <zjsyhjh@gmail.com>
2016-09-08 09:11:47 +0800

    add extra field

--- src/jvm/rtda/heap/object.go
+++ src/jvm/rtda/heap/object.go
@@ -1,8 +1,12 @@
 package heap
 
+/*
+ * extra字段用来记录Object结构体实例的格外信息
+ */
 type Object struct {
 	class *Class
 	data  interface{}
+	extra interface{}
 }
 
 func newObject(class *Class) *Object {
@@ -20,6 +24,14 @@ func (self *Object) Fields() Slots {
 	return self.data.(Slots)
 }
 
+func (self *Object) Extra() interface{} {
+	return self.extra
+}
+
+func (self *Object) SetExtra(extra interface{}) {
+	self.extra = extra
+}
+
 func (self *Object) IsInstanceOf(class *Class) bool {
 	return class.isAssignableFrom(self.class)
 }
@@ -28,4 +40,10 @@ func (self *Object) SetRefVar(name, descriptor string, ref *Object) {
 	field := self.class.getField(name, descriptor, false)
 	slots := self.data.(Slots)
 	slots.SetRef(field.slotID, ref)
+}
+
+func (self *Object) GetRefVar(name, descriptor string) *Object {
+	field := self.class.getField(name, descriptor, false)
+	slots := self.data.(Slots)
+	return slots.GetRef(field.slotID)
 }
