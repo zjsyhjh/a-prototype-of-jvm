92424930549f632d79f0951fd385936e67dba388
Jiahuan <zjsyhjh@gmail.com>
2016-09-09 18:18:03 +0800

    implement loadArrayClass function

--- src/jvm/rtda/heap/class_loader.go
+++ src/jvm/rtda/heap/class_loader.go
@@ -18,23 +18,93 @@ type ClassLoader struct {
 }
 
 func NewClassLoader(classPath *classpath.ClassPath, verboseFlag bool) *ClassLoader {
-	return &ClassLoader{
+	loader := &ClassLoader{
 		classPath:   classPath,
 		classMap:    make(map[string]*Class),
 		verboseFlag: verboseFlag,
 	}
+	loader.loadBasicClasses()
+	loader.loadPrimitiveClasses()
+	return loader
 }
 
 /*
- * 加载类，先查找是否已经加载，若是，则返回类；否则加载非数组类(普通类)
+ * 先加载java.lang.Class类
+ * 将已经加载好的类关联类对象
+ */
+func (self *ClassLoader) loadBasicClasses() {
+	jClassClass := self.LoadClass("java/lang/Class")
+	for _, class := range self.classMap {
+		class.jClass = jClassClass.NewObject()
+		class.jClass.extra = class
+	}
+}
+
+/*
+ * 加载基本类型类, void, int, double等，基本类型没有超类，也实现任何接口
+ */
+func (self *ClassLoader) loadPrimitiveClasses() {
+	for primitiveType, _ := range primitiveTypes {
+		self.loadPrimitiveClass(primitiveType)
+	}
+}
+func (self *ClassLoader) loadPrimitiveClass(className string) {
+	class := &Class{
+		accessFlags: ACC_PUBLIC,
+		className:   className,
+		classLoader: self,
+		initStarted: true,
+	}
+	class.jClass = self.classMap["java/lang/Class"].NewObject()
+	class.jClass.extra = class
+	self.classMap[className] = class
+}
+
+/*
+ * 加载类，先查找是否已经加载，若是，则返回类
+ * 如果是数组类，则加载数组类，否则就加载普通类
+ * 看java.lang.Class类是否已经加载，如果是，则给类关联类对象
  */
 func (self *ClassLoader) LoadClass(className string) *Class {
 	if class, ok := self.classMap[className]; ok {
 		return class
 	}
-	return self.loadNonArrayClass(className)
+	var class *Class
+	if className[0] == '[' {
+		class = self.loadArrayClass(className)
+	} else {
+		class = self.loadNonArrayClass(className)
+	}
+
+	if jClassClass, ok := self.classMap["java/lang/Class"]; ok {
+		class.jClass = jClassClass.NewObject()
+		class.jClass.extra = class
+	}
+	return class
 }
 
+/*
+ * 加载数组类
+ */
+func (self *ClassLoader) loadArrayClass(className string) *Class {
+	class := &Class{
+		accessFlags: ACC_PUBLIC,
+		className:   className,
+		classLoader: self,
+		initStarted: true,
+		superClass:  self.LoadClass("java/lang/Object"),
+		interfaces: []*Class{
+			self.LoadClass("java/lang/Cloneable"),
+			self.LoadClass("java/io/Serializable"),
+		},
+	}
+	self.classMap[className] = class
+	return class
+}
+
+/*
+ * 加载普通类(非数组类)
+ */
 func (self *ClassLoader) loadNonArrayClass(className string) *Class {
 	data, entry := self.readClass(className)
 	class := self.defineClass(data)
@@ -82,7 +152,7 @@ func parseClass(data []byte) *Class {
  */
 func resolveSuperClass(class *Class) {
 	if class.className != "java/lang/Object" {
-		fmt.Printf("className : " + class.className + " superClassName : " + class.superClassName + "\n")
+		//fmt.Printf("className : " + class.className + " superClassName : " + class.superClassName + "\n")
 		class.superClass = class.classLoader.LoadClass(class.superClassName)
 	}
 }
@@ -95,9 +165,11 @@ func resolveInterfaces(class *Class) {
 	if interfaceCount > 0 {
 		class.interfaces = make([]*Class, interfaceCount)
 		for i, interfaceName := range class.interfaceNames {
-			fmt.Printf("ClassName : " + class.className)
-			fmt.Printf(" InterfaceName : " + interfaceName)
-			fmt.Println()
+			/*
+				fmt.Printf("ClassName : " + class.className)
+				fmt.Printf(" InterfaceName : " + interfaceName)
+				fmt.Println()
+			*/
 			class.interfaces[i] = class.classLoader.LoadClass(interfaceName)
 		}
 	}
@@ -137,9 +209,9 @@ func calInstanceFieldSlotID(class *Class) {
 		if !field.IsStatic() {
 			field.slotID = slotID
 			slotID++
-		}
-		if field.isLongOrDouble() {
-			slotID++
+			if field.isLongOrDouble() {
+				slotID++
+			}
 		}
 	}
 	class.instanceSlotCount = slotID
@@ -154,9 +226,9 @@ func calStaticFieldSlotID(class *Class) {
 		if field.IsStatic() {
 			field.slotID = slotID
 			slotID++
-		}
-		if field.isLongOrDouble() {
-			slotID++
+			if field.isLongOrDouble() {
+				slotID++
+			}
 		}
 	}
 	class.staticSlotCount = slotID
@@ -169,7 +241,7 @@ func allocAndInitStaticVars(class *Class) {
 	class.staticVars = newSlots(class.staticSlotCount)
 	for _, field := range class.fields {
 		if field.IsStatic() && field.IsFinal() {
-			initStaticVars(class, field)
+			initStaticFinalVars(class, field)
 		}
 	}
 }
@@ -177,7 +249,7 @@ func allocAndInitStaticVars(class *Class) {
 /*
  * 从常量池中加载常量池，然后给静态变量赋值
  */
-func initStaticVars(class *Class, field *Field) {
+func initStaticFinalVars(class *Class, field *Field) {
 	vars := class.staticVars
 	cp := class.constantPool
 	cpIndex := field.ConstantValueIndex()
@@ -198,7 +270,10 @@ func initStaticVars(class *Class, field *Field) {
 			value := cp.GetConstant(cpIndex).(float64)
 			vars.SetDouble(slotID, value)
 		case "Ljava/lang/String;":
-			panic("todo")
+			fmt.Println("Ljava/lang/String;")
+			goStr := cp.GetConstant(cpIndex).(string)
+			javaStr := JString(class.Loader(), goStr)
+			vars.SetRef(slotID, javaStr)
 		}
 	}
 }
