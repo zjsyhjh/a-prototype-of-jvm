74f0846ce55073e11a6eb94f3d34ceabfe8dd23f
Jiahuan <zjsyhjh@gmail.com>
2016-09-14 15:13:49 +0800

    add String test

---
+++ src/example/StringTest2.java
@@ -0,0 +1,16 @@
+public class StringTest2 {
+    
+    public static void main(String[] args) {
+    	String s1 = "1";
+    	String s2 = "2";
+    	System.out.println(s1 + s2);
+
+    	String s3 = "xx";
+    	String s4 = "xx";
+    	System.out.println(s3 == s4);
+
+    	String s5 = "x";
+    	String s6 = s5 + "y";
+    	System.out.println(s6);
+    }
+}
