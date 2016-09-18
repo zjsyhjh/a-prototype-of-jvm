401c2c5afffd182873757495e5f52cc7f0ef6cf2
Jiahuan <zjsyhjh@gmail.com>
2016-09-18 19:13:45 +0800

    add Object test

---
+++ src/example/ObjectTest.java
@@ -0,0 +1,9 @@
+public class ObjectTest {
+
+	public static void main(String[] args) {
+		Object  obj1 = new Object();
+		Object  obj2 = new Object();
+
+		System.out.println(obj1.hashCode());
+	}
+}
