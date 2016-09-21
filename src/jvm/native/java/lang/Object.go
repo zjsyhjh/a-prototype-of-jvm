c9b8931d40c9048bbce670cd5133ae633160d216
Jiahuan <zjsyhjh@gmail.com>
2016-09-21 18:43:32 +0800

    add native clone method

--- src/jvm/native/java/lang/Object.go
+++ src/jvm/native/java/lang/Object.go
@@ -12,7 +12,7 @@ import (
 func init() {
 	native.Register("java/lang/Object", "getClass", "()Ljava/lang/Class;", getClass)
 	native.Register("java/lang/Object", "hashCode", "()I", hashCode)
-
+	native.Register("java/lang/Object", "clone", "()Ljava/lang/Object;", clone)
 }
 
 /*
@@ -26,9 +26,25 @@ func getClass(frame *rtda.Frame) {
 
 /*
  * public native int hashCode()
+ * 把对象引用（Object结构体指针）转换成uintptr类型，然后强制转换成int32推入操作数栈顶
  */
 func hashCode(frame *rtda.Frame) {
 	this := frame.LocalVars().GetThis()
 	hash := int32(uintptr(unsafe.Pointer(this)))
 	frame.OperandStack().PushInt(hash)
+}
+
+/*
+ * protected native Object clone() throws CloneNotSupportedException;
+ * 对象克隆
+ */
+func clone(frame *rtda.Frame) {
+	this := frame.LocalVars().GetThis()
+	cloneable := this.Class().Loader().LoadClass("java/lang/Cloneable")
+
+	if !this.Class().IsImplements(cloneable) {
+		panic("java.lang.CloneNotSupportedException")
+	}
+
+	frame.OperandStack().PushRef(this.Clone())
 }
